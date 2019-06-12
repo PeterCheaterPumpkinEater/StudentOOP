df9a6f61da4d0b401b67c0f89bafab5854b88542
Peter Liu <peter51198@gmail.com>
2019-06-12 12:43:42 -0400

    Update date.cpp

--- my_code/date.cpp
+++ my_code/date.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+Date::Date(int d, int m, int y) : day(d), month(m), year(y) {};
+
 ostream& operator<<(ostream& os, const Date& date){
     os << "Day: " << date.day << " Month: " << date.month << " Year: " << date.year;
     return os;

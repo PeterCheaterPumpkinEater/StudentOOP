ac08bacee98b7a4322b56d2f7f87fcd5c53b31da
Peter Liu <peter51198@gmail.com>
2019-06-12 12:43:40 -0400

    Update weather.cpp

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -4,6 +4,73 @@
 
 using namespace std;
 
+const double F_TO_C = 5 / 9;
+const double C_TO_F = 9 / 5;
+
+Image::Image(int w, int h, std::string flnm)
+: width(w), height(h)
+{
+    filename = flnm;
+    image_buf = new char[image_sz()];
+}
+
+// copy constructor:
+Image::Image(const Image& img2) {
+    width = img2.width;
+    height = img2.height;
+    filename = img2.filename;
+    image_buf = new char[image_sz()];
+    for (int i = 0; i < image_sz(); i++) {
+        image_buf[i] = img2.image_buf[i];
+    }
+}
+
+// destructor
+Image::~Image() {
+    if (image_buf != nullptr) {
+        delete image_buf;
+    }
+}
+
+// assignment operator:
+Image& Image::operator=(const Image& img2) {
+    if (&img2 == this) {
+        return *this;
+    }
+    width = img2.width;
+    height = img2.height;
+    filename = img2.filename;
+    
+    delete image_buf;
+    image_buf = new char[image_sz()];
+    for (int i = 0; i < image_sz(); i++) {
+        image_buf[i] = img2.image_buf[i];
+    }
+    
+    return *this;
+}
+
+int Image::image_sz() {
+    return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+    width = img2.width;
+    height = img2.height;
+    filename = img2.filename;
+}
+
+//--------------------------------------------------------
+
+/*
+ * Setting `display() = 0` here makes this an abstract
+ * class that can't be implemented.
+ * */
+string Image::display(std::string s) {
+    return "Displaying image " + s;
+}
+
 /*
  * A constructor for weather class.
  * */
@@ -16,6 +83,22 @@ string Weather::get_name()const {
 
 int Weather::get_rating()const{
     return rating;
+}
+
+pair<double, double> WReading::get_heat_index()const{
+    return make_pair(temperature, humidity);
+}
+
+pair<double, double> WReading::get_wind_chill()const{
+    return make_pair(temperature, windspeed);
+}
+
+double WReading::get_temp()const{
+    return temperature;
+}
+
+double WReading::get_tempF()const{
+    return temperature * 9/5 + 32;
 }
 
 void Weather::set_rating(int new_rating) {
